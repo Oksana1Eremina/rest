d83e24170808a1827d6ca4462027c341d3272e8b
Oksana Eremina <oksana@yandex.ru>
2023-09-07 22:13:33 +0300

    add logging and exception processing

--- soap_api.py
+++ soap_api.py
@@ -1,16 +1,24 @@
 import yaml
+import logging
 from zeep import Client, Settings
 
-
-with open('config.yaml', encoding='utf-8') as f:
-    data = yaml.safe_load(f)
-    wsdl = data('wsdl')
+try:
+    with open('config.yaml', encoding='utf-8') as f:
+        logging.info('trying to open file config.yaml')
+        data = yaml.safe_load(f)
+        wsdl = data('wsdl')
+    logging.info('file config.yaml successfully opened')
+except Exception(FileNotFoundError) as e:
+    logging.exception('open file config.yaml failed')
 
 settings = Settings(strict=False)
 client = Client(wsdl=wsdl, settings=settings)
 
 
 def check_text(text: str) -> list[str]:
-    resp = client.service.checkText(text)
-    return resp[0]['s']
-
+    try:
+        logging.info('try to send rest request')
+        resp = client.service.checkText(text)
+        return resp[0]['s']
+    except Exception as ex:
+        logging.exception('exception when try to send rest request')
